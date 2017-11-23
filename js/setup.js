6ff79fbc72bda42d4f66caac2976d730d56bf79f
Maksim <maxsimshiba@yandex.ru>
2017-11-23 19:29:12 +0300

    Правки наставника

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,14 @@
 var WIZARD_COUNT = 4;
 var wizards = [];
 
+// Находим элемент на странице
+var userDialog = document.querySelector('.setup');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
+
 // Функция вывода случайного индекса массива.
 // Принимает длинну массива
 function randomNumber(length) {
@@ -25,13 +33,13 @@ function renderWizard(wizard) {
 }
 
 // Заполненение массива объектами
-function fillArrWizards(length) {
+function fillArrWizards(wizardCount) {
   var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var WIZARD_FAMILES = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
   var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-  for (var i = 0; i < length; i++) {
+  for (var i = 0; i < wizardCount; i++) {
     wizards.push({
       name: WIZARD_NAMES[randomNumber(WIZARD_NAMES.length)] + WIZARD_FAMILES[randomNumber(WIZARD_FAMILES.length)],
       coatColor: WIZARD_COAT_COLOR[randomNumber(WIZARD_COAT_COLOR.length)],
@@ -42,18 +50,11 @@ function fillArrWizards(length) {
   return wizards;
 }
 
-// Находим элемент на странице
-var userDialog = document.querySelector('.setup');
+
 // Показываем элемент
 userDialog.classList.remove('hidden');
 
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
 fillArrWizards(WIZARD_COUNT);
-
-var fragment = document.createDocumentFragment();
 
 for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
